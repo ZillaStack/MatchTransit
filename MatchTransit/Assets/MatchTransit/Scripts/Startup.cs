72e903eb1b8ea5ff56c456eb94edbcd8c4728742
Jason Holland <jholland918@gmail.com>
2024-07-13 11:48:42 -0500

    init

---
+++ MatchTransit/Assets/MatchTransit/Scripts/Startup.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.MatchTransit.Scripts
+{
+    public class Startup
+    {
+        public void ConfigureServices()
+        {
+
+        }
+    }
+}
